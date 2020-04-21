4346cf8d83264283a2432af6e6f2d9f1c09dd390
André Gesser <45247030+andregesser95@users.noreply.github.com>
2020-04-21 16:16:53 -0300

    Segunda parte

--- alura-typer/public/js/placar.js
+++ alura-typer/public/js/placar.js
@@ -1,3 +1,5 @@
+$("#botao-placar").click(mostraPlacar);
+
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "André";
@@ -15,21 +17,25 @@ function novaLinha(usuario, numPalavras) {
     var colunaUsuario = $("<td>").text(usuario);
     var colunaPalavras = $("<td>").text(numPalavras);
     var colunaRemover = $("<td>");
-    
+
     var link = $("<a>").addClass("botao-remover").attr("href", "#");
     var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
 
     link.append(icone);
     colunaRemover.append(link);
-    
+
     linha.prepend(colunaRemover);
     linha.prepend(colunaPalavras);
     linha.prepend(colunaUsuario);
 
-    return linha;    
+    return linha;
 }
 
 function removeLinha() {
     event.preventDefault();
     $(this).parent().parent().remove();
+}
+
+function mostraPlacar() {
+    $(".placar").toggle();
 }
